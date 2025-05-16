cac55ea7a3389e1ca80d26dbe156adc6b1ecf29b
roosterzz <agarciahotmail9@gmail.com>
2025-05-15 21:55:21 -0700

    added cityMusicData.js for mock data

---
+++ src/cityMusicData.js
@@ -0,0 +1,32 @@
+export const cityMusicData = {
+    "Los Angeles" : {
+        lat: 34.0522,
+        long: -118.2437,
+        topArtists: ["Billie Eilish", "Kendrick Lamar", "The Weeknd", "Lady Gaga", "Bruno Mars"],
+        topSongs: ["Birds of a Feather", "Not Like Us", "Espresso", "Who", "Good Luck, Babe!"] 
+    },
+    "San Francisco" : {
+        lat: 37.7749,
+        long: -122.4194,
+        topArtists: ["Green Day", "Lorde", "Blackbear", "Too Short", "Train"],
+        topSongs: ["Boulevard of Broken Dreams", "Royals", "Hot Girl Bummer", "Blow the Whistle", "Hey, Soul Sister"]
+    },
+    "San Diego" : {
+        lat: 32.7157,
+        long: -117.1611,
+        topArtists: ["Blink-182", "Slightly Stoopid", "Switchfoot", "Jason Mraz", "AViVA"],
+        topSongs: ["All The Small Things", "Closer to the Sun", "Dare You to Move", "I'm Yours", "WICKED"]
+    },
+    "Sacramento" : {
+        lat: 38.5816,
+        long: -121.4944,
+        topArtists: ["Mozzy", "Cake", "Hobo Johnson", "Deftones", "Brotha Lynch Hung"],
+        topSongs: ["Sleep Walkin", "The Distance", "Peach Scone", "Change", "Rest in Piss"]
+    },
+    "Fresno" : {
+        lat: 36.7378,
+        long: -119.7871,
+        topArtists: ["Fashawn", "Patrick Contreras", "Stephen Marley", "Kali Uchis", "J Boog"],
+        topSongs: ["The Ecology", "Electric", "No Cigarette Smoking", "After the Storm", "Let's Do It Again"]
+    },
+}
