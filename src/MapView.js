c52312d977732e0bc314580e453f3f5220215bf1
roosterzz <agarciahotmail9@gmail.com>
2025-05-16 12:01:34 -0700

    updated map to be fixed at one position, only california

--- src/MapView.js
+++ src/MapView.js
@@ -4,6 +4,14 @@ import { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
 import { GeoJSON } from 'react-leaflet';
 import { californiaGeoJson } from './californiaBorder';
+import L from 'leaflet';
+
+const musicIcon = new L.Icon({
+    iconUrl: '/public/music-note.png',
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30]
+});
 
 function MapView() {
     const mapRef = useRef();
@@ -21,10 +29,13 @@ return (
     minZoom={5}
     maxZoom={10}
     scrollWheelZoom={true}
-    style={{ height: '100%', width: '100%' }}>
-    maxBounds ={[[32.0, -125.0],[42.0, -114.0]]}  [// California Borders]
+    style={{ height: '100%', width: '100%' }}
+    maxBounds ={[[32.0, -125.0],[42.0, -114.0]]}  // California Borders
     maxBoundsViscosity={1.0}
-    worldCopyJump={false} [// prevents repeated maps]
+    worldCopyJump={false} // prevents repeated maps
+    ref={mapRef}
+    >
+   
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
